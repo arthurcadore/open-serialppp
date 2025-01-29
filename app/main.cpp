736e52d4ec4d65f338a41dd5085a5606718affdb
arthurcadore <arthurbarcella.ifsc@gmail.com>
2025-01-29 11:06:29 -0300

    :construction: added new code to main

--- app/main.cpp
+++ app/main.cpp
@@ -2,38 +2,23 @@
 
 using namespace std;
 
-void tx (Serial & rf, char * msg) {
-  // esta é a mensagem a ser transmitida
-  vector<char> buffer(msg, msg + strlen(msg));
-
-  // envia a mensagem
-  int n = rf.write(buffer);
-
-  cout << "Enviou " << n << " bytes" << endl;
-}
-
-void rx (Serial & rf) {
-  // recebe até 32 octetos
-  auto buffer = rf.read(32);
-
-  cout << "Recebeu " << buffer.size() << " bytes: ";
-
-  // mostra na tela os caracteres recebidos
-  cout.write(buffer.data(), buffer.size());
-
-  cout << endl;
-}
 
 int main (int argc, char * argv[]) {
     
-  Serial Tx(argv[1], B9600);
-  Serial Rx(argv[1], B9600);
+  Serial serial(argv[1], B9600);
 
-    while(true){
-        char msg[100];
-        cout << "Digite a mensagem a ser enviada: ";
-        cin.getline(msg, 100);
-        tx(Tx, msg);
-        rx(Rx);
-    }
+  class ConcreteAplicacao : public Aplicacao {
+    // Implement all pure virtual functions here
+  };
+
+  ConcreteAplicacao app;
+  Enquadramento enq(serial, 9600)
+  
+  enq.conecta($app);
+
+  Poller sched;
+  sched.adiciona(&enq);
+  sched.adiciona(&app);
+
+  sched.despache();
 }
--- app/main.h
+++ app/main.h
@@ -4,10 +4,14 @@
 #include <iostream>
 #include <cstring>
 #include "serial.h"
+#include "poller.h"
+#include "aplicacao.h"
+#include "enquadramento.h"
 
 void tx (Serial & rf, char * msg);
 void rx (Serial & rf);
 
 int main (int argc, char * argv[]);
 
-#endif    /* __MAIN_H__ */
+
+#endif    
